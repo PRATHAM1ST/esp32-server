7cd22a0f00ef343646d30720a48b506512171b54
Pratham <chudasamapratham142@gmail.com>
2023-02-05 04:48:49 +0530

    schema updated

--- schema.js
+++ schema.js
@@ -11,7 +11,7 @@ const dataSchema = new mongoose.Schema({
 	req: String,
 	when: Number,
 	file: String,
-	body: { humidity: Number, temp: Number },
+	body: Object,
 	best_location_type: String,
 	best_location_when: Number,
 	best_lat: Number,
